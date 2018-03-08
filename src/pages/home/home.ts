a98f51a40a30ea23f60c31fa204903c2f5ef7ca3
ramonparaiba <ramonparaiba@gmail.com>
2018-03-08 03:45:01 -0300

    Layout e lógica decide Combustivel

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public precoGas:number;
+  public precoEtanol:number;
+  public razao:string;
+  
   constructor(public navCtrl: NavController) {
 
   }
+  calcula(){
+    let calculo = this.precoEtanol/this.precoGas;
+    this.razao = calculo.toString();
+    if(calculo >= 0.75){
+      this.razao = calculo.toString()+" Bote gai que é melhor";
+    }else{
+      this.razao = calculo.toString()+" Aico é melhor";
+    }
+  }
+  limpar(){
+    this.razao ="";
+  }
+
 
 }
